cff438ac22359a8abe08462cbdc56c66a3709e80
Bjarke Thorn Carstens <stealthy_stealth@hotmail.com>
2015-09-16 14:42:07 +0200

    added filtering functionality

--- p7/src/main/main.java
+++ p7/src/main/main.java
@@ -1,6 +1,7 @@
 package main;
 
 import streaming.OurStatusListener;
+import twitter4j.FilterQuery;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 
@@ -9,13 +10,24 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		OurStatusListener listener = new OurStatusListener(); 
-		
+		OurStatusListener listener = new OurStatusListener();
 		TwitterStream stream = new TwitterStreamFactory().getInstance(); 
 		stream.addListener(listener);
 		
-		stream.sample();
-		
+		//stream.sample();
+
+        double[][] locations = new double[][]{
+                {-74,40},
+                {-73,41}
+        };
+
+
+        FilterQuery query = new FilterQuery();
+        //query.language("da");
+        query.locations(locations);
+        //-74,40,-73,41
+        stream.filter(query);
+
 	}
 
 }
--- p7/src/streaming/OurStatusListener.java
+++ p7/src/streaming/OurStatusListener.java
@@ -1,19 +1,96 @@
 package streaming;
-import twitter4j.*;
-public class OurStatusListener implements StatusListener{
 
-	public void onStatus(Status status ){
-		
-		
-		
-	}
-	
-	 public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
-     public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
-     public void onException(Exception ex) {
-         ex.printStackTrace();
-     }
-     public void onScrubGeo(long x, long y) {}
-     public void onStallWarning(StallWarning warning) {}
-     
+import twitter4j.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class OurStatusListener implements StatusListener {
+
+    public void onStatus(Status status) {
+        GeoLocation geo = status.getGeoLocation();
+
+        if (geo != null) {
+            if (containsKeywords(status.getText())) {
+                System.out.println("\n" + status.getUser().getScreenName() + " wrote: ");
+                System.out.println(status.getText());
+
+                if (status.getPlace() != null) {
+                    if (status.getPlace().getFullName().equals("New York, NY")) {
+                        System.out.println(status.getText());
+                        System.out.println("Country: " + status.getPlace().getCountry());
+                        System.out.println("Place: " + status.getPlace().getFullName());
+                    }
+                }
+
+                double latitude = geo.getLatitude();
+                double longitude = geo.getLongitude();
+
+                //System.out.println("Place boundingbox " + status.getPlace().getBoundingBoxCoordinates());
+                System.out.println("location:");
+                System.out.println("latitude: " + latitude + " , " + "longitude: " + longitude);
+            }
+            else {
+                System.out.println("Tweet: " + status.getId() + " removed");
+            }
+        }
+
+
+
+        /*
+        HashtagEntity[] array = status.getHashtagEntities();
+		if(array.length != 0)
+            System.out.println(array[0].getText());
+		*/
+    }
+
+    public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+        System.out.println("User: " + statusDeletionNotice.getUserId() + " deleted");
+    }
+
+    public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+    }
+
+    public void onException(Exception ex) {
+        ex.printStackTrace();
+    }
+
+    public void onScrubGeo(long x, long y) {
+    }
+
+    public void onStallWarning(StallWarning warning) {
+    }
+
+
+    private boolean containsKeywords(String tweetText) {
+        tweetText = tweetText.toLowerCase();
+
+        List<String> keywords = new ArrayList<String>();
+        keywords.add("food");
+        keywords.add("poison");
+        keywords.add("restaurant");
+        keywords.add("sick");
+        keywords.add("soup");
+        keywords.add("drink");
+        keywords.add("bed");
+        keywords.add("hungry");
+        keywords.add("soda");
+        keywords.add("chinese food");
+        keywords.add("chipotle");
+        keywords.add("mcdonald");
+        keywords.add("mc donald");
+        keywords.add("burgerking");
+        keywords.add("burger king");
+        keywords.add("stomach pain");
+        keywords.add("diarrhea");
+        keywords.add("the shits");
+
+        for (String keyword : keywords) {
+            if (tweetText.contains(keyword))
+                return true;
+        }
+        return false;
+    }
+
 }
