fb83fe9f962e02f18233ece9d9b1aaf0901b37a5
unknown <Mat_F_S@live.dk>
2015-09-18 16:00:17 +0200

    Fixed the insert query function. Instead of the executeQuery statement, i used executeUpdate, as the query expects a resultset, which we ofcourse do not get, as we are just inserting. I also use a PreparedStatement instead of a Statement, as PreparedStatements encodes the symbols for us, so we do not need to worry about it(escape characters etc). This also means if someone tweets an SQL query, it will not get executed, which it will using a normal statement, so in general it is just good practice i guess. The reason it prints the database has been closed twice, is because we close it in the insert function, and in the main where it is called from. I do not know if this is neccessary or not, but easy to fix if it is the case.

--- p7/src/dataAccessLayer/DBInsert.java
+++ p7/src/dataAccessLayer/DBInsert.java
@@ -1,6 +1,7 @@
 package dataAccessLayer;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Date;
@@ -15,14 +16,38 @@ public class DBInsert {
 		this.connection = connection;
 	}
 	
-	public void Insert(long tweetID, long userID, long responseID, long retweetID, String tweet, Date date, double lat, double lon, String keywords)
+	public void Insert(int tweetID, int userID, int responseID, int retweetID, String tweet, Date date, double lat, double lon, String keywords)
 	{
 		Connection con = connection.getInstance().getDBcon();
 		Timestamp timestamp = new Timestamp(date.getTime()); 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 		try {
-			Statement st = con.createStatement();
-			st.executeQuery("INSERT INTO tweets VALUES (" + tweetID 				+ ", " 
+			
+			String insertSQL= "INSERT INTO tweets" +
+									"(tweetID, userID, responseID, retweetID, tweet, createAt, lat, lon, keywords) VALUES" +
+									"(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement pst = con.prepareStatement(insertSQL);
+			pst.setInt(1, tweetID);
+			pst.setInt(2, userID);
+			pst.setInt(3, responseID);
+			pst.setInt(4, retweetID);
+			pst.setString(5, tweet);
+			pst.setString(6, sdf.format(timestamp));
+			pst.setDouble(7, lat);
+			pst.setDouble(8, lon);
+			pst.setString(9, keywords);
+			pst.executeUpdate();
+		}
+		catch(Exception E) {
+			System.out.println("Mads' mor");
+			E.printStackTrace();
+		}
+		DBConnect.closeConnection();
+	}
+}
+/*
+Statement st = con.createStatement();
+st.executeUpdate("INSERT INTO tweets VALUES (" + tweetID 				+ ", " 
 			  											  + userID 					+ ", " 
 														  + responseID 				+ ", " 
 														  + retweetID 				+ ", \'" 
@@ -32,11 +57,4 @@ public class DBInsert {
 														  + lon 					+ ", \'" 
 														  + keywords + "\'"
 														  + ")");
-		}
-		catch(Exception E) {
-			System.out.println("Mads' mor");
-			E.printStackTrace();
-		}
-		DBConnect.closeConnection();
-	}
-}
+*/
