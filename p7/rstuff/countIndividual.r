f5ad31389cbd3f2a11edc9894b70a1294b7d07c6
btcstealth <stealthy_stealth@hotmail.com>
2015-09-30 13:27:07 +0200

    update for r filters

--- p7/rstuff/countIndividual.r
+++ p7/rstuff/countIndividual.r
@@ -8,15 +8,19 @@ seperateKeywords = function(keywords,textTable){
 	for(i in 1:length(keywords)){
 		answerTable = table(sapply(textTable,helper,keyword = keywords[i]))
 		nrTable = answerTable[names(answerTable) == TRUE]
-		nr = nrTable[[1]]
-		z[i] = nr
+		if(0 == length(nrTable))
+			z[i] = 0
+		else{
+			nr = nrTable[[1]]
+			z[i] = nr
+		}
 	}
 	z	
 }
 
 onlyText = sqldf("SELECT tweettext FROM tweets")
 onlyTextVector = as.vector(onlyText[[1]])
-keywords = scan("keywords.txt",what="",sep=",")
+keywords = scan("eh.txt",what="",sep=",")
 
 tester = seperateKeywords(keywords,onlyTextVector)  
 barplot(tester,names.arg = keywords)
