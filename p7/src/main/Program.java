69f977e85a93dadcf4a47e45dcefd1d2cd65032f
unknown <Mat_F_S@live.dk>
2015-10-05 18:16:32 +0200

    Added the preprocessor class to handle preprocessing of tweets. It includes processing of links, mentions and symbols in tweets. Added examples in the main(commented)

--- p7/src/main/Program.java
+++ p7/src/main/Program.java
@@ -1,6 +1,7 @@
 package main;
 
 import dataAccessLayer.DBConnect;
+import modelLayer.Tweet;
 import modelLayer.TweetStorage;
 
 import streaming.Oauth;
@@ -10,15 +11,38 @@ import twitter4j.FilterQuery;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import businessLogicLayer.Preprocessor;
 import businessLogicLayer.TweetQueryThread;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		/*
+		Tweet tweet1 = new Tweet(0, 2, 3, 4, "tweet1 Mads er diarrhea,:-: @food @urdad lol headache!! http://t.co/urmom hhah l0l n1..", new Date(), 0.2, 0.5);
+		System.out.println("Original Tweet1: " + tweet1.getTweetText());
+		Preprocessor.processTweetLinks(tweet1);
+		System.out.println("Links Processed Tweet1: " + tweet1.getTweetText());
+		Preprocessor.processTweetMentions(tweet1);
+		System.out.println("Mentions Processed Tweet1: " + tweet1.getTweetText());
+		Preprocessor.processTweetSymbols(tweet1);
+		System.out.println("Symbols Processed Tweet1: " + tweet1.getTweetText() + "\n");
+		
+		Tweet tweet2 = new Tweet(0, 2, 3, 4, "tweet2 Mads er diarrhea,:-: @food @urdad lol headache!! http://t.co/urmom hhah l0l n1..", new Date(), 0.2, 0.5);
+		System.out.println("Original Tweet2: " + tweet2.getTweetText());
+		Preprocessor.processWholeTweet(tweet2);
+		System.out.println("Whole Processed Tweet2: " + tweet2.getTweetText() + "\n");
+	
+		Tweet tweet3 = new Tweet(0, 2, 3, 4, "tweet3 Mads er diarrhea,:-: @food @urdad lol headache!! http://t.co/urmom hhah l0l n1..", new Date(), 0.2, 0.5);
+		System.out.println("Original Tweet3: " + tweet3.getTweetText());
+		Preprocessor.processTweet(tweet3);;
+		System.out.println("Whole Processed Tweet3: " + tweet3.getTweetText());
+		*/
+		
 		Oauth auth = new Oauth(); 
 		OurStatusListener listener = new OurStatusListener();
 		TwitterStreamFactory tsf = new TwitterStreamFactory(auth.createConfigBuilder().build());
