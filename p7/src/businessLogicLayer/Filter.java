f20fe4296c50780411a878e481bcd24d8ec6a7a8
btcstealth <stealthy_stealth@hotmail.com>
2015-10-12 13:41:53 +0200

    changed filter to use Map instead of list

--- p7/src/businessLogicLayer/Filter.java
+++ p7/src/businessLogicLayer/Filter.java
@@ -11,7 +11,7 @@ public class Filter {
 
 	public static TweetStorage filterTweet(TweetStorage tweets, Date date) {
 		TweetStorage newTweetStorage = new TweetStorage();
-		List<String> regs = getRegularExpressions();
+		Map<String,String> regs = getRegularExpressions();
 		String reg1 = ".*"; 		// Any character 0-many times
 		String reg2 = "\\s@?"; 		// space followed by a @ zero or one time
 		String reg3 = ""; 			// Any of the regular expressions in regs(list of regular expressions)					
@@ -21,8 +21,10 @@ public class Filter {
 			if (tweet.getCreatedAt().before(date)) {
 				break;
 			}
-			for (String reg : regs) {
-				reg3 = reg;
+
+			for (Map.Entry<String, String> entry : regs.entrySet())
+			{
+				reg3 = entry.getValue();
 				// Full Regex Example:
 				// ".*\\s@?(bad?\\s?|upset?\\s?)?stoma(ch|k)\\s?(pain?|flu?|flue?|)\\w?\\s"
 				Pattern p = Pattern.compile(reg1 + reg2 + reg3 + reg4, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
@@ -37,19 +39,20 @@ public class Filter {
 
 	public static Map<String, Integer> countOccurances(TweetStorage tweets){
 		Map<String, Integer> counters = new HashMap<String, Integer>();
-		List<String> regularExpressions = getRegularExpressions();
+		Map<String, String> regularExpressions = getRegularExpressions();
 
 		String reg1 = ".*"; 		// Any character 0-many times
 		String reg2 = "\\s@?"; 		// space followed by a @ zero or one time
-		String reg3 = ""; 			// Any of the regular expressions in regs(list of regular expressions)
 		String reg4 = "\\w?\\s?"; 	// Any letter or digit zero or one time followed by a space
 		for (int i = tweets.size() - 1; i >= 0; i--) {
 			Tweet tweet = tweets.get(i);
-			for (String reg : regularExpressions) {
-				reg3 = reg;
+
+			for (Map.Entry<String, String> entry : regularExpressions.entrySet())
+			{
+				String regEx = entry.getValue();
 				// Full Regex Example:
 				// ".*\\s@?(bad?\\s?|upset?\\s?)?stoma(ch|k)\\s?(pain?|flu?|flue?|)\\w?\\s"
-				Pattern p = Pattern.compile(reg1 + reg2 + reg3 + reg4, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+				Pattern p = Pattern.compile(reg1 + reg2 + regEx + reg4, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 				Matcher m = p.matcher(tweet.getTweetText());
 				if (m.find()) {
 					// do counter work here
@@ -61,24 +64,24 @@ public class Filter {
 	}
 
 
-	static private List<String> getRegularExpressions() {
-		List<String> regs = new ArrayList<String>();
-		regs.add("fo(od|d|ood|ods|odd)\\s?poi(son|sons|sen|sens|sn)");
-		regs.add("si(ck|k)");
-		regs.add("(ill|il|fever|pain)");
-		regs.add("(bad?\\s?|upset?\\s?)?stoma(ch|k)\\s?(pain?|flu?|flue?|)");
-		regs.add("dia(rrhea|rria|rhea|ria|hrrhea|hrhea)");
-		regs.add("de(hy|hi)dra(tion|sion)");
-		regs.add("salmonel(la|a)");
-		regs.add("nausea");
-		regs.add("vom(it|mit)");
-		regs.add("throw(ing|)\\s?(u|o)p");
-		regs.add("pe(p|b)to\\s?bi(s|ss)mal");
-		regs.add("(on\\s?the\\s?)?toilet");
-		regs.add("(c|k)ram(p|b)s");
-		regs.add("infe(c|k)(t|s)ion");
-		regs.add("d(i|e)sease");
-		regs.add("head(a|e)(che|k)");
+	static private Map<String, String> getRegularExpressions() {
+		Map<String,String> regs = new HashMap<String, String>();
+		regs.put("food", "fo(od|d|ood|ods|odd)\\s?poi(son|sons|sen|sens|sn)");
+		regs.put("sick", "si(ck|k)");
+		regs.put("ill", "(ill|il|fever|pain)");
+		regs.put("stomach flue","(bad?\\s?|upset?\\s?)?stoma(ch|k)\\s?(pain?|flu?|flue?|)");
+		regs.put("diarrhea","dia(rrhea|rria|rhea|ria|hrrhea|hrhea)");
+		regs.put("dehydration", "de(hy|hi)dra(tion|sion)");
+		regs.put("salmonella", "salmonel(la|a)");
+		regs.put("nausea", "nausea");
+		regs.put("vomit", "vom(it|mit)");
+		regs.put("throwing up", "throw(ing|)\\s?(u|o)p");
+		regs.put("pepto bismal", "pe(p|b)to\\s?bi(s|ss)mal");
+		regs.put("on the toilet","(on\\s?the\\s?)?toilet");
+		regs.put("cramps","(c|k)ram(p|b)s");
+		regs.put("infection","infe(c|k)(t|s)ion");
+		regs.put("decrease","d(i|e)sease");
+		regs.put("headache","head(a|e)(che|k)");
 
 		return regs;
 	}
