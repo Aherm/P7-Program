fd3b3ec6c71ccb7c8f1f26949e7f1789965732de
Aherm <anders.hermansen1991@gmail.com>
2015-10-23 19:32:39 +0200

    Changed ordering of Keyword methods to be consistent with other classes

--- p7/src/modelLayer/Keyword.java
+++ p7/src/modelLayer/Keyword.java
@@ -4,41 +4,34 @@ public class Keyword {
 	private String name;
 	private String regex;
 	private int weight;
-	
-	public Keyword(String name, String regex, int weight)
-	{
+
+	public Keyword(String name, String regex, int weight) {
 		this.name = name;
 		this.regex = regex;
 		this.weight = weight;
 	}
-	
-	public String getName()
-	{
+
+	public String getName() {
 		return name;
 	}
-	
-	public int getWeight()
-	{
-		return weight;
-	}
-	
-	public String getRegex()
-	{
-		return regex;
-	}
-	
-	public void setName(String name)
-	{
+
+	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public void setWeight(int cost)
-	{
-		this.weight = cost;
+
+	public String getRegex() {
+		return regex;
 	}
-	
-	public void setRegex(String regex)
-	{
+
+	public void setRegex(String regex) {
 		this.regex = regex;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int cost) {
+		this.weight = cost;
 	}
 }
