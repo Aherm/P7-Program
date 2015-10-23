804274485cd2d85a305ebdd5b94e2e1d4c07cd62
Aherm <anders.hermansen1991@gmail.com>
2015-10-23 20:00:11 +0200

    Refactored Tweet

--- p7/src/modelLayer/Tweet.java
+++ p7/src/modelLayer/Tweet.java
@@ -1,171 +1,155 @@
 package modelLayer;
 
+import twitter4j.Status;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-
-import twitter4j.Status;
 
 public class Tweet {
+	private long tweetID, userID, responseID, retweetID;
+	private String tweetText;
+	private Date createdAt;
+	private double lat, lon;
+	private List<Keyword> matchedKeywords = new ArrayList<Keyword>();
 
-    private long tweetID, userID, responseID, retweetID;
-    private String tweetText;
-    private Date createdAt;
-    private double lat, lon;
-    private double score = -1;
-    private List<Keyword> matchedKeywords = new ArrayList<Keyword>();    
-    private Cluster assignedCluster = null;
-    private boolean addedToStorage = false;
+	private double score = -1;
+	private Cluster assignedCluster = null;
+	private boolean addedToStorage = false;
 
-    public Tweet() {
-    }
+	public Tweet() {}
 
-    public Tweet(long tweetID, long userID, long responseID, long retweetID, String tweetText, Date createdAt) {
-        this(tweetID, userID, responseID, retweetID, tweetText, createdAt, 0.0, 0.0);
-    }
+	public Tweet(long tweetID, long userID, long responseID, long retweetID, String tweetText, Date createdAt) {
+		this(tweetID, userID, responseID, retweetID, tweetText, createdAt, 0.0, 0.0);
+	}
 
-    public Tweet(long tweetID, long userID, long responseID, long retweetID, String tweetText, Date createdAt, double lat, double lon) {
-        this(tweetID, userID, responseID, retweetID, tweetText, createdAt, lat, lon, new ArrayList<Keyword>());
-    }
-    
-    public Tweet(long tweetID, long userID, long responseID, long retweetID, String tweetText, Date createdAt, double lat, double lon, List<Keyword> keywords) {
-        this.tweetID = tweetID;
-        this.userID = userID;
-        this.responseID = responseID;
-        this.retweetID = retweetID;
-        this.tweetText = tweetText;
-        this.createdAt = createdAt;
-        this.lat = lat;
-        this.lon = lon;
-        this.matchedKeywords = keywords;
-    }
+	public Tweet(long tweetID, long userID, long responseID, long retweetID, String tweetText, Date createdAt, double lat, double lon) {
+		this.tweetID = tweetID;
+		this.userID = userID;
+		this.responseID = responseID;
+		this.retweetID = retweetID;
+		this.tweetText = tweetText;
+		this.createdAt = createdAt;
+		this.lat = lat;
+		this.lon = lon;
+	}
 
-    public static Tweet createTweet(Status status) {
-        //In case the tweet is not geotagged
-        if (status.getGeoLocation() == null){
-            return new Tweet(status.getId(), status.getUser().getId(), status.getInReplyToUserId(), status.getCurrentUserRetweetId(),
-                    status.getText(), status.getCreatedAt());
-        }
-        else return new Tweet(status.getId(), status.getUser().getId(), status.getInReplyToUserId(), status.getCurrentUserRetweetId(),
-                status.getText(), status.getCreatedAt(), status.getGeoLocation().getLatitude(), status.getGeoLocation().getLongitude());
-    }
-    
-    public void add(Keyword keyword)
-    {
-    	matchedKeywords.add(keyword);
-    }
-    
-    public long getTweetID() {
-        return tweetID;
-    }
+	public static Tweet createTweet(Status status) {
+		if (status.getGeoLocation() == null) {
+			return new Tweet(status.getId(), status.getUser().getId(), status.getInReplyToUserId(), status.getCurrentUserRetweetId(),
+					status.getText(), status.getCreatedAt());
+		}
+		else return new Tweet(status.getId(), status.getUser().getId(), status.getInReplyToUserId(), status.getCurrentUserRetweetId(),
+				status.getText(), status.getCreatedAt(), status.getGeoLocation().getLatitude(), status.getGeoLocation().getLongitude());
+	}
 
-    public void setTweetID(long tweetID) {
-        this.tweetID = tweetID;
-    }
+	public long getTweetID() {
+		return tweetID;
+	}
 
-    public long getUserID() {
-        return userID;
-    }
+	public void setTweetID(long tweetID) {
+		this.tweetID = tweetID;
+	}
 
-    public void setUserID(long userID) {
-        this.userID = userID;
-    }
+	public long getUserID() {
+		return userID;
+	}
 
-    public long getResponseID() {
-        return responseID;
-    }
+	public void setUserID(long userID) {
+		this.userID = userID;
+	}
 
-    public void setResponseID(long responseID) {
-        this.responseID = responseID;
-    }
+	public long getResponseID() {
+		return responseID;
+	}
 
-    public long getRetweetID() {
-        return retweetID;
-    }
+	public void setResponseID(long responseID) {
+		this.responseID = responseID;
+	}
 
-    public void setRetweetID(long retweetID) {
-        this.retweetID = retweetID;
-    }
+	public long getRetweetID() {
+		return retweetID;
+	}
 
-    public String getTweetText() {
-        return tweetText;
-    }
+	public void setRetweetID(long retweetID) {
+		this.retweetID = retweetID;
+	}
 
-    public void setTweetText(String tweetText) {
-        this.tweetText = tweetText;
-    }
+	public String getTweetText() {
+		return tweetText;
+	}
 
-    public Date getCreatedAt() {
-        return createdAt;
-    }
+	public void setTweetText(String tweetText) {
+		this.tweetText = tweetText;
+	}
 
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
+	public Date getCreatedAt() {
+		return createdAt;
+	}
 
-    public double getLat() {
-        return lat;
-    }
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
 
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
+	public double getLat() {
+		return lat;
+	}
 
-    public double getLon() {
-        return lon;
-    }
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
 
-    public void setLon(double lon) {
-        this.lon = lon;
-    }
+	public double getLon() {
+		return lon;
+	}
 
-    public double getScore()
-    {
-    	score = 0;
-    	for(Keyword keyword : matchedKeywords)
-    	{
-    		this.score += keyword.getWeight();
-    	}
-    	
-    	return score;
-    }
-    
-    public void resetScore()
-    {
-    	score = -1;
-    }
-    
-    public void setScore(double score) {
-    	this.score = score;
-    }
-    
-    public Cluster getCluster() {
-    	return this.assignedCluster;
-    }
-    
-    public void setCluster (Cluster c) {
-    	this.assignedCluster = c;
-    }
-    
-    public boolean isGeotagged(){
-    	if(lat == 0 && lon == 0)
-    		return false; 
-    	else 
-    		return true; 
-    }
-    
-    public Tweet clone() {
-    	Tweet res = new Tweet(tweetID, userID, responseID, retweetID, tweetText, createdAt, lat, lon, matchedKeywords);
-    	res.setCluster(assignedCluster);
-    	res.setScore(score);
-    	return res;
-    }
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
 
-    public boolean isAddedToStorage() {
-        return addedToStorage;
-    }
+	public void add(Keyword keyword)
+	{
+		matchedKeywords.add(keyword);
+	}
 
-    public void setAddedToStorage(boolean addedToStorage) {
-        this.addedToStorage = addedToStorage;
-    }
+	public double getScore()
+	{
+		score = 0;
+		for(Keyword keyword : matchedKeywords)
+		{
+			this.score += keyword.getWeight();
+		}
+
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+	public Cluster getCluster() {
+		return this.assignedCluster;
+	}
+
+	public void setCluster (Cluster c) {
+		this.assignedCluster = c;
+	}
+
+	public boolean isAddedToStorage() {
+		return addedToStorage;
+	}
+
+	public void setAddedToStorage(boolean addedToStorage) {
+		this.addedToStorage = addedToStorage;
+	}
+
+	public boolean isGeotagged(){
+		if(lat == 0 && lon == 0)
+			return false; 
+		else 
+			return true; 
+	}
+
+	public Tweet clone() {
+		return new Tweet(tweetID, userID, responseID, retweetID, tweetText, createdAt, lat, lon);
+	}
 }
