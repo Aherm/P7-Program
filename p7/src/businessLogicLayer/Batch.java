fddff776d0a4847c368bf06fe9b8162c18b7aadf
Aherm <anders.hermansen1991@gmail.com>
2015-10-24 12:26:18 +0200

    Cleaned up batch (but do we even use that one?

--- p7/src/businessLogicLayer/Batch.java
+++ p7/src/businessLogicLayer/Batch.java
@@ -2,48 +2,53 @@ package businessLogicLayer;
 
 import dataAccessLayer.DBGetTweets;
 import modelLayer.TweetStorage;
-import java.util.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+//TODO: Do we even use this class any more? Might want to remove it.
 public class Batch {
 
-	public static TweetStorage filterTweets(){
-		TweetStorage ts = new TweetStorage(); 
-		TweetStorage interval = new TweetStorage(); 
-		int size = 10000; 
-		int start = 0; 
-		int itnr = 1; 
-		do{
+	public static TweetStorage filterTweets() {
+		TweetStorage ts = new TweetStorage();
+		TweetStorage interval = new TweetStorage();
+		int size = 10000;
+		int start = 0;
+		int itnr = 1;
+		do {
 			System.out.println("iteration: " + itnr);
 			interval = DBGetTweets.getInterval(start, size);
 			ts.addAll(Filter.filterTweets(interval, new Date()));
-		    System.out.println(ts.size());
+			System.out.println(ts.size());
 			start = start + size; 
 			itnr++;
-		}while(interval.size() == size); 
-		
+		} while(interval.size() == size); 
+
 		return ts;
 	}
 
-	public static Map<String, Integer> getTweets(){
+	public static Map<String, Integer> getTweets() {
 		Map<String, Integer> matchedKeywords = new HashMap<String, Integer>();
 		TweetStorage interval = new TweetStorage();
 		int size = 10000;
 		int start = 0;
 		int itnr = 1;
-		do{
+		do {
 			System.out.println("iteration: " + itnr);
 			interval = DBGetTweets.getInterval(start, size);
 			Map<String, Integer> tempMap = Filter.countMatches(interval);
-			for (Map.Entry entry : tempMap.entrySet()){
+			for (Map.Entry<String, Integer> entry : tempMap.entrySet()) {
 
 				String key = (String) entry.getKey();
 				int value = (Integer) entry.getValue();
 
 				if (matchedKeywords.get(key) == null ){
 					matchedKeywords.put(key, value);
-				}else {
+				}
+				else {
 					matchedKeywords.put(key, matchedKeywords.get(key) + value);
 				}
+				
 				System.out.println("------------------------------------------------------------------------------------");
 				System.out.println("Local matched");
 				System.out.println(matchedKeywords);
@@ -54,9 +59,8 @@ public class Batch {
 			System.out.println("start value " + start);
 			start = start + size;
 			itnr++;
-		}while(interval.size() == size);
+		} while(interval.size() == size);
 
 		return matchedKeywords;
 	}
-
 }
