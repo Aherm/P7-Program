d3ff128c574f56d101d3abd5c1f23970dbcd9783
Aherm <anders.hermansen1991@gmail.com>
2015-11-25 10:48:23 +0100

    Added scoring for restaurants.

--- p7/src/modelLayer/Restaurant.java
+++ p7/src/modelLayer/Restaurant.java
@@ -1,8 +1,14 @@
 package modelLayer;
 
+import java.util.List;
+
+import businessLogicLayer.Filter;
+
 public class Restaurant implements OurLocation {
 	private String name;
 	private double lat, lon;
+	private TweetStorage matchedTweets = new TweetStorage();
+	private int score = -1;
 	
 	public Restaurant (String name, double lat, double lon) {
 		this.name = name;
@@ -32,5 +38,26 @@ public class Restaurant implements OurLocation {
 
 	public void setLon(double lon) {
 		this.lon = lon;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+	
+	public int calculateScore(Grid g, InvertedIndex ii, double dist) {
+		this.matchedTweets = g.rangeQuery(this, dist);
+		TweetStorage temp = Filter.restaurantNameQuery(this, ii);
+		// Removes first to make sure the same tweet isn't there twice.
+		this.matchedTweets.removeAll(temp);
+		this.matchedTweets.addAll(temp);
+		
+		this.score = matchedTweets.size();
+		return this.score;
+	}
+	
+	public static void scoreAll(List<Restaurant> list, Grid g, InvertedIndex ii, double dist) {
+		for (Restaurant r : list) {
+			r.calculateScore(g, ii, dist);
+		}
 	}
 }
