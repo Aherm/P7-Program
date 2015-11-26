d6cb540f38aacdfe2a6b3550de20c47fa7095d06
ExpReg <madsriis92@gmail.com>
2015-11-26 22:08:23 +0100

    updated the inverted index to include alternative spellings of the things we need

--- p7/src/modelLayer/InvertedIndex.java
+++ p7/src/modelLayer/InvertedIndex.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InvertedIndex extends HashMap<String, Set<Tweet>> {
 	// This field has to be there. We don't use it.
@@ -17,9 +19,19 @@ public class InvertedIndex extends HashMap<String, Set<Tweet>> {
     }
     
     public void addIndex(Tweet tweet) {
+    	
     	for(String word : this.keySet())
     	{
-    		if(tweet.getTweetText().contains(" " + word + " "))
+    		String regex1 = "(@|\\s?|#)"+ word +"(nyc|ny|\\s?)"; 
+    		Pattern p = Pattern.compile(regex1, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+            Matcher m = p.matcher(tweet.getTweetText()); 
+            boolean test1 = m.find(); 
+            String regex2 = "(@|\\s?|#)"+ word.replaceAll("\\s+", "") +"(nyc|ny|\\s?)"; 
+            p = Pattern.compile(regex2, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+            m = p.matcher(tweet.getTweetText()); 
+            boolean test2 = m.find(); 
+    		
+            if(test1 || test2)
     			this.get(word).add(tweet);
     	}
     }
