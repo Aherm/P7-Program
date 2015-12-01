0517e9793e925e89999c911ac6b2f6c9b949d373
Aherm <anders.hermansen1991@gmail.com>
2015-12-01 11:08:49 +0100

    Updated the scoring methods to take the correct data structure

--- p7/src/businessLogicLayer/Scoring.java
+++ p7/src/businessLogicLayer/Scoring.java
@@ -3,26 +3,35 @@ package businessLogicLayer;
 import java.util.HashMap;
 import java.util.Map;
 
+import modelLayer.Grid;
+import modelLayer.InvertedIndex;
 import modelLayer.Restaurant;
-import modelLayer.TweetStorage;
 
 public class Scoring {
 	private static Map<Restaurant,Double> geoScore = new HashMap<Restaurant,Double>();
 	private static Map<Restaurant,Double> wordScore = new HashMap<Restaurant,Double>();
 	
-	public static double geotaggedScore(Restaurant r, TweetStorage tweets) {
+	public static double geotaggedScore(Restaurant r, Grid grid) {
 		double result = 0;
+		
+		
 		geoScore.put(r, result);
 		return result;
 	}
 	
-	public static double keywordScore(Restaurant r, TweetStorage tweets) {
+	public static double keywordScore(Restaurant r, InvertedIndex ii) {
 		double result = 0;
 		wordScore.put(r, result);;
 		return result;
 	}
 	
-	public static double combinedScore(Restaurant r, TweetStorage tweets) {
+	public static double combinedScore(Restaurant r, Grid grid, InvertedIndex ii) {
+		if(!geoScore.containsKey(r)) {
+			geotaggedScore(r, grid);
+		}
+		if(!wordScore.containsKey(r)) {
+			keywordScore(r, ii);
+		}
 		double result = geoScore.get(r) + wordScore.get(r);
 		return result;
 	}
