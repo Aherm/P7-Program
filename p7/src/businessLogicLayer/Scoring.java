4670293c4bc1bb578ebc1ab01991f6bd1af22212
Aherm <anders.hermansen1991@gmail.com>
2015-12-01 11:04:38 +0100

    Updated scores to hash maps

--- p7/src/businessLogicLayer/Scoring.java
+++ p7/src/businessLogicLayer/Scoring.java
@@ -1,26 +1,29 @@
 package businessLogicLayer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import modelLayer.Restaurant;
 import modelLayer.TweetStorage;
 
 public class Scoring {
-	private static double geoScore;
-	private static double wordScore;
+	private static Map<Restaurant,Double> geoScore = new HashMap<Restaurant,Double>();
+	private static Map<Restaurant,Double> wordScore = new HashMap<Restaurant,Double>();
 	
 	public static double geotaggedScore(Restaurant r, TweetStorage tweets) {
 		double result = 0;
-		geoScore = result;
+		geoScore.put(r, result);
 		return result;
 	}
 	
 	public static double keywordScore(Restaurant r, TweetStorage tweets) {
 		double result = 0;
-		wordScore = result;
+		wordScore.put(r, result);;
 		return result;
 	}
 	
 	public static double combinedScore(Restaurant r, TweetStorage tweets) {
-		double result = geoScore + wordScore;
+		double result = geoScore.get(r) + wordScore.get(r);
 		return result;
 	}
 }
