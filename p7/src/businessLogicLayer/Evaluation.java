7a6dd2e256a6633557c2af56affea785a0421a8d
Aherm <anders.hermansen1991@gmail.com>
2015-12-02 15:04:50 +0100

    Added part of the roc curve

--- p7/src/businessLogicLayer/Evaluation.java
+++ p7/src/businessLogicLayer/Evaluation.java
@@ -1,5 +1,10 @@
 package businessLogicLayer;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+import modelLayer.Tweet;
 import modelLayer.TweetStorage;
 
 public class Evaluation {
@@ -10,5 +15,36 @@ public class Evaluation {
 		for(int i = 0; i < 10; i++){
 			
 		}
+	}
+	
+	public void rocCurve(TweetStorage tweets) {
+		List<Point> points = new ArrayList<Point>();
+		for (int threshold = 100; threshold >= 0; threshold--) {
+			int TP = 0, FP = 0, TN = 0, FN = 0;
+			for (Tweet t : tweets) {
+				// if classified = true = expected TP++
+				// if classified = true != expected FP++
+				// if classified = false = expected TN++
+				// if classified = false != expected FN++
+			}
+			// TP rate
+			// FP rate
+		}
+	}
+	
+	public double precision(int TP, int FP) {
+		return TP / (TP + FP);
+	}
+	
+	public double recall(int TP, int FN) {
+		return TP / (TP + FN);
+	}
+	
+	public double tpRate(int TP, int FN) {
+		return recall(TP, FN);
+	}
+	
+	public double tnRate(int FP, int TN) {
+		return TN / (FP + TN);
 	}
 }
