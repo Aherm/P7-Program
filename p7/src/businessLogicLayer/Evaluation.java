aaf99051f5965bc0e8550576c93d56257acc0dc4
expreg <madsriis92@gmail.com>
2015-12-02 13:43:37 +0100

    stuff

---
+++ p7/src/businessLogicLayer/Evaluation.java
@@ -0,0 +1,14 @@
+package businessLogicLayer;
+
+import modelLayer.TweetStorage;
+
+public class Evaluation {
+
+	public void tenFoldCrossValidation(TweetStorage trainingSet){
+		TweetStorage[] folds = new TweetStorage[10];
+		
+		for(int i = 0; i < 10; i++){
+			
+		}
+	}
+}
