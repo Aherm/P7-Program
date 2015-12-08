0bc6f78fdffdd684e68b749b6088275c76d6d69f
expreg <madsriis92@gmail.com>
2015-12-08 17:04:59 +0100

    stuff

--- p7/src/evaluationLayer/RankHandler.java
+++ p7/src/evaluationLayer/RankHandler.java
@@ -10,12 +10,13 @@ public class RankHandler {
 	public List<Rank> ourRanks; 
 	public List<Rank> dohmRanks; 
 	
-	public RankHandler(List<Rank> y, List<Rank> y2){
-		this.ourRanks = y; 
-		this.dohmRanks = y2; 
+	public RankHandler(List<Rank> dohmh, List<Rank> our){
+		this.ourRanks = our; 
+		this.dohmRanks = dohmh; 
 	}
 	
 	public void printRanks(){
+		removeNotIn();  
 		Collections.sort(dohmRanks,new ScoreCompare());
 		Collections.sort(ourRanks, new ScoreCompare());
 		setRanks(dohmRanks);
@@ -24,7 +25,7 @@ public class RankHandler {
 		for(Rank r : dohmRanks){
 			for(Rank ourR : ourRanks){
 				if(r.getRestaurant().equals(ourR.getRestaurant())){
-					System.out.println(r.getRestaurant().getName() + " dohmh: " + r.getRank() + " our: " + r.getRank()); 
+					System.out.println(r.getRestaurant().getName() + " dohmh: " + r.getRank() + " our: " + ourR.getRank()); 
 				}
 			}
 		}		
@@ -37,16 +38,27 @@ public class RankHandler {
 		int currentRank = 1;
 		
 		for(Rank r : ranks){
-			
 			if(r.getScore() != lastScore){
 				currentRank++; 
 				lastScore = r.getScore(); 
 			}
-			
 			r.setRank(currentRank);
 		}
 	}
 	
+	private void removeNotIn(){
+		List<Rank> rankList = new ArrayList<Rank>();
+		
+		for(Rank r : dohmRanks){
+			for(Rank our : ourRanks){
+				if(r.getRestaurant().equals(our.getRestaurant())){
+					rankList.add(r);
+				}
+			}
+		}
+		
+		dohmRanks = rankList; 
+	}
 	
 	
 }
--- p7/src/evaluationLayer/Simulation.java
+++ p7/src/evaluationLayer/Simulation.java
@@ -19,6 +19,7 @@ import modelLayer.TweetStorage;
 import streaming.TwitterRest;
 import twitter4j.Status;
 import twitter4j.TwitterException;
+import utility.Tuple;
 
 public class Simulation {
 	
@@ -27,13 +28,17 @@ public class Simulation {
 	// TODO: Determine the best number of rows and columns. Maybe pass them as argument instead.
 	private Grid grid = new Grid(-78, -70, 38, 44, 1000, 1000);
 	private List<Restaurant> restaurants; 
+	private List<Rank> ranks; 
 	Scoring score = new Scoring(); 
 	
 	public void stream(){
 		DBConnect connection = DBConnect.getInstance();
         connection.connectToServer("jdbc:postgresql://172.25.26.208/", "world" + "", "postgres", "21");
 		TweetStorage allTweet = DBGetTweets.getAllTweetsExperiment(); 
-		restaurants = DBGetRestaurants.getRestaurants().x; 
+		Tuple<List<Restaurant>,List<Rank>> tuple = DBGetRestaurants.getRestaurants(); 
+		restaurants = tuple.x; 
+		ranks = tuple.y; 
+		
 		
 		for(Restaurant r: restaurants){
     		if(!r.getName().contains("v {iv}") && !r.getName().contains("floor)"))
@@ -78,10 +83,15 @@ public class Simulation {
 	}
 	
 	private void evaluate(){
+		List<Rank> ourRanks =  new ArrayList<Rank>();
 		for(Restaurant r: restaurants){
 			if(score.geotaggedScore(r, grid) > 0)
-				System.out.println(r.getName() + ": " + score.geotaggedScore(r, grid));
+				ourRanks.add(new Rank(r,score.geotaggedScore(r, grid)));
 		}
+		
+		RankHandler hanlder = new RankHandler(ranks, ourRanks);
+		
+		hanlder.printRanks();
 	}
 	
 }
